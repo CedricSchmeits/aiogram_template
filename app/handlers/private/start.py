f82106ccf7b70e29f81182cbb8b69a6433b1c85c
0kit <kostyajog@mail.ru>
2020-08-25 22:32:03 +0300

    throttling example

--- app/handlers/private/start.py
+++ app/handlers/private/start.py
@@ -2,8 +2,10 @@ from aiogram import types
 from aiogram.dispatcher.filters import CommandStart
 
 from app.loader import dp
+from app.utils.misc.rate_limit import rate_limit
 
 
+@rate_limit(30, 'command_start')
 @dp.message_handler(CommandStart())
 async def process_start(msg: types.Message):
     await msg.answer(f'Hello, {msg.from_user.full_name}!')
