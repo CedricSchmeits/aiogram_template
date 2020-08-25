ce5cdd4ad4a05f28a664f5e276b3afd8ecc4eeb3
0kit <kostyajog@mail.ru>
2020-08-25 22:30:35 +0300

    logging middleware was added

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,4 +1,5 @@
 from aiogram import Dispatcher
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from loguru import logger
 
 from .throttling import ThrottlingMiddleware
@@ -6,4 +7,5 @@ from .throttling import ThrottlingMiddleware
 
 async def setup(dp: Dispatcher):
     dp.middleware.setup(ThrottlingMiddleware())
+    dp.middleware.setup(LoggingMiddleware())
     logger.info('Middlewares are successfully configured')
