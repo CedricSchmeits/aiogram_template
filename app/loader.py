a4797bbe983922a1d3b7779372cc767983fd117f
0kit <kostyajog@mail.ru>
2020-08-26 15:50:28 +0300

    redis storage

--- app/loader.py
+++ app/loader.py
@@ -1,11 +1,12 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+
 from app.db_api.database import db
 
 from app import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
+storage = RedisStorage2()
 dp = Dispatcher(bot, storage=storage)
 
 __all__ = (
