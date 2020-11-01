a6a23b74a06da0aa66a6fa1700194b4efe8496f4
0Kit <kostyajog@mail.ru>
2020-11-01 17:11:52 +0300

    default commands fix

--- app/utils/default_commands.py
+++ app/utils/default_commands.py
@@ -4,6 +4,6 @@ from loguru import logger
 
 async def setup_default_commands(dp):
     await dp.bot.set_my_commands([
-        types.BotCommand("developer", "Разработчик"),
+        types.BotCommand("start", "start"),
     ])
     logger.info('Standard commands are successfully configured')
