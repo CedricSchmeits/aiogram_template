786917f79f75fae3e2b3a21d9ba920f69570d3a9
0Kit <kostyajog@mail.ru>
2020-11-19 20:25:10 +0300

    update post-generation text

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
         remove_file('Makefile')
 
     print("Edit .env file before start")
-    print("Create poetry environment by command:\n    poetry update")
+    print("Create poetry environment by command:\n    poetry install")
     print("The project is successfully created!")
