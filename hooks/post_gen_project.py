d85350d81a25872522fd00ae8fc113b25792c516
0Kit <kostyajog@mail.ru>
2020-11-21 21:56:31 +0300

    colorized answer

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,13 @@ import os
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m[WARNING]: "
+INFO = "\x1b[1;33m[INFO]: "
+HINT = "\x1b[3;33m"
+SUCCESS = "\x1b[1;32m[SUCCESS]: "
+ERROR = "\033[31m[ERROR]: "
+
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
@@ -14,5 +21,7 @@ if __name__ == '__main__':
     if '{{ cookiecutter.add_makefile }}' != 'y':
         remove_file('Makefile')
 
-    print("\nYou can see guide at the https://github.com/0Kit/aiogram_template/blob/cookiecutter/README.md")
-    print("Project {{ cookiecutter.project_name }} was successfully created\n")
+    print(HINT, "\nYou can see guide at the https://github.com/0Kit/aiogram_template/blob/cookiecutter/README.md",
+          TERMINATOR)
+    print(SUCCESS + "Project {{ cookiecutter.project_name }} was successfully created\n",
+          TERMINATOR)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,34 +3,39 @@ import sys
 from pathlib import Path
 
 
-# TODO Create module for hooks regexes
+# REGEXES
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 VERSION_REGEX = r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$'
 
-
-# TODO Create module for hooks exceptions
-class ProjectNameError(Exception):
-    ...
-
-
-class ProjectVersionError(Exception):
-    ...
+# COLORS
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m[WARNING]: "
+INFO = "\x1b[1;33m[INFO]: "
+HINT = "\x1b[3;33m"
+SUCCESS = "\x1b[1;32m[SUCCESS]: "
+ERROR = "\033[31m[ERROR]: "
 
 
 def validate_module_name(module_name: str):
     if not re.match(MODULE_REGEX, module_name):
-        raise ProjectNameError(
-            f'ERROR: The project name ({module_name}) is not a valid Python module name. '
-            'Please do not use a - and use _ instead'
+        print(
+            ERROR,
+            f'The project name "{module_name}" is not a valid Python module name. '
+            'Please do not use a - and use _ instead',
+            TERMINATOR
         )
+        exit(1)
 
 
 def validate_version(version: str):
     if not re.match(VERSION_REGEX, version):
-        raise ProjectVersionError(
-            f'ERROR: The verison ({version}) is not a valid Python version. '
-            'Read PEP440 for more information'
+        print(
+            ERROR,
+            f'The version "{version}" is not a valid Python version. '
+            'Read PEP440 for more information',
+            TERMINATOR
         )
+        exit(1)
 
 
 if __name__ == '__main__':
