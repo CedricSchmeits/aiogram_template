ffb65217d32bdbea0d6863590e6548b00d41e945
0Kit <kostyajog@mail.ru>
2020-11-22 19:06:31 +0300

    broadcaster fix

--- {{cookiecutter.project_name}}/app/utils/notify_admins.py
+++ {{cookiecutter.project_name}}/app/utils/notify_admins.py
@@ -5,6 +5,6 @@ from loguru import logger
 from app.utils import Broadcast
 
 
-async def notify_admins(admins: Option[List[int], List[str], int, str]):
+async def notify_admins(admins: Union[List[int], List[str], int, str]):
     count = await (Broadcast(admins, 'The bot is running!')).start()
     logger.info(f"{count} admins received messages")
