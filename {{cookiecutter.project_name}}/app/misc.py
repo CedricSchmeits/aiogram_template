13542d88c945d8b151f9e9cfeeb8d59a1b728470
0Kit <kostyajog@mail.ru>
2020-11-22 12:47:48 +0300

    use_redis

--- {{cookiecutter.project_name}}/app/misc.py
+++ {{cookiecutter.project_name}}/app/misc.py
@@ -1,5 +1,11 @@
 from aiogram import Bot, Dispatcher, types
+
+{%- if cookiecutter.use_redis == "y" %}
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
+{%- else %}
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+{%- endif %}
+
 from gino import Gino
 
 from app import config
@@ -10,12 +16,14 @@ bot = Bot(
     token=config.BOT_TOKEN,
     parse_mode=types.ParseMode.HTML,
 )
-
+{% if cookiecutter.use_redis == "y" %}
 storage = RedisStorage2(
     host=config.REDIS_HOST,
     port=config.REDIS_PORT,
 )
-
+{% else %}
+storage = MemoryStorage()
+{% endif %}
 dp = Dispatcher(
     bot=bot,
     storage=storage,
